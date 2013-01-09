3dee7f1acba6bd700f439aa9aad6d6c5895f3e77
Roy Paulsen <roy.paulsen@royp.net>
2013-01-09 16:24:07 +0100

    Now using 2 spaces as indentation (as default in xmllint). Also removes line breaks in the selected XML string, to make the result more pretty.

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # sublimetext_tidyxml
-
 Formatting XML in Sublime Text 2 using xmllint
 
 Based upon this blog post: http://www.bergspot.com/blog/2012/05/formatting-xml-in-sublime-text-2-xmllint/
 
 ## Installation
+To be able to use this plugin you need __xmllint__, which is provided by default in OS X and Linux.
 
-To be able to use this plugin you need xmllint, which is provided by default in OS X and Linux.
-I guess ut would be possible to install it in Cygwin in Windows.
+In Windows you need to [download xmllint for Windows](http://code.google.com/p/xmllint/downloads/list).
 
-__To install the plugin:__
+### To install the plugin:
 
 1. Go to your Sublime Text plugins directory
     * To find it, you can open Sublime Text and then: Preferences &rarr; Browse packages...
 1. Clone the git repo into a folder "Tidy XML"
     * `git clone git@github.com:roypa/sublimetext_tidyxml.git "Tidy XML"`
 1. Done, now select XML and press Cmd + K, Cmd + F (OS X)
+
+### Problems?
+
+If the command is not working, have a look at the status line.
+Any error messages from xmllint will be shown there.
--- tidy_xml_lint.py
+++ tidy_xml_lint.py
@@ -1,13 +1,22 @@
 import sublime, sublime_plugin, subprocess
+import re
 
 class TidyXmlLintCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    command = "XMLLINT_INDENT=$'\t' xmllint --format --encode utf-8 -"
+    # Setting the environment variable XMLLINT_INDENT controls the indentation. The default value is two spaces " ".
+    # To use TAB for indentation, prefix command with: XMLLINT_INDENT=$'\t'
+    command = "xmllint --format --noblanks --encode utf-8 -"
 
     # help from http://www.sublimetext.com/forum/viewtopic.php?f=2&p=12451
     xmlRegion = sublime.Region(0, self.view.size())
+
+    xmlString = self.view.substr(self.view.sel()[0])
+    # Remove new line characters
+    xmlString = re.sub('\r\n?|\n', '', xmlString)
+
+    # Run xmllint command
     p = subprocess.Popen(command, bufsize=-1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
-    result, err = p.communicate(self.view.substr(self.view.sel()[0]).encode('utf-8'))
+    result, err = p.communicate(xmlString.encode('utf-8'))
 
     if err != "":
       self.view.set_status('xmllint', "xmllint: "+err)
@@ -18,4 +27,3 @@ class TidyXmlLintCommand(sublime_plugin.TextCommand):
 
   def clear(self):
     self.view.erase_status('xmllint')
-
